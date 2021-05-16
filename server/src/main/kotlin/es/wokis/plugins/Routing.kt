3d1111e6973f21c2d7d7a4a15edd354c2571dcfa
Wikijito7 <wikyfg@gmail.com>
2021-05-16 11:49:01 +0200

    Rutas
    - Implementadas las rutas de profile y /user/username

--- server/src/main/kotlin/es/wokis/plugins/Routing.kt
+++ server/src/main/kotlin/es/wokis/plugins/Routing.kt
@@ -1,27 +1,29 @@
 package es.wokis.plugins
 
 import es.wokis.data.dto.LoginUserDTO
-
+import es.wokis.data.dto.RegisterUserDTO
 import es.wokis.data.repository.UserRepository
 import es.wokis.utils.user
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.auth.*
-import io.ktor.response.*
+import io.ktor.http.*
 import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun Application.configureRouting() {
+    val userRepository = UserRepository.getInstance()
 
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
 
+        // Login / Register
+
         post("/login") {
             val user = call.receive<LoginUserDTO>()
-            print("user is $user")
-            val token: String? = UserRepository().login(user)
+            val token: String? = userRepository.login(user)
 
             if (token != null) {
                 call.respond(HttpStatusCode.OK, token)
@@ -30,12 +32,63 @@ fun Application.configureRouting() {
             }
         }
 
+        post("/register") {
+            val user = call.receive<RegisterUserDTO>()
+
+            val token: String? = userRepository.register(user)
+
+            if (token != null) {
+                call.respond(HttpStatusCode.OK, token)
+            } else {
+                call.respond(HttpStatusCode.Conflict, "That user already exists")
+            }
+
+        }
+
+        // si no está autorizado, 401.
         authenticate {
             route("/asd") {
                 get("auth") {
-                    call.respondText("Auth!? name = ${call.user?.username}" +
-                            ", pass= ${call.user?.password}")
+                    call.respondText(
+                        "Auth!? name = ${call.user?.username}" +
+                                ", pass= ${call.user?.password}"
+                    )
                 }
+            }
+
+            // perfil del usuario logueado
+            get("/profile") {
+                val user = call.user
+
+                if (user != null) {
+                    val fullUser = userRepository.getUser(user.username)
+
+                    if (fullUser != null) {
+                        call.respond(HttpStatusCode.OK, fullUser)
+                    }
+                }
+
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+            // perfil usuario x, TODO: Añadir roles.
+            get("/user/{username}") {
+                val username = call.parameters["username"]
+
+                if (username.isNullOrBlank()) {
+                    call.respond(HttpStatusCode.BadRequest, "Username must be given.")
+
+                } else {
+                    val user = userRepository.getUser(username)
+
+                    if (user != null) {
+                        call.respond(user)
+
+                    } else {
+                        call.respond(HttpStatusCode.NotFound, username)
+                    }
+                }
+
             }
         }
     }
