cd3b9fa1e1ec447e0dc64bd54373691ee3e4d3d0
wikijito7 <wikyfg@gmail.com>
2021-05-25 12:59:16 +0200

    Imagenes
    - Añadida la imagen por defecto e implementada.

--- server/src/main/kotlin/es/wokis/plugins/Images.kt
+++ server/src/main/kotlin/es/wokis/plugins/Images.kt
@@ -2,15 +2,23 @@ package es.wokis.plugins
 
 import io.ktor.application.*
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 
 fun Application.initImages() {
     val imagePath = config.getString("imagepath")
     val imageFolder = File(imagePath)
+
+    val defaultImagePath = this::class.java.getResource("/default.png") ?: throw IllegalAccessException()
+    val defaultImage = Paths.get(File(defaultImagePath.toURI()).path)
 
     if (!imageFolder.exists()) {
         imageFolder.mkdirs()
 
         File("$imagePath/logos").mkdirs()
         File("$imagePath/avatars").mkdirs()
+
+        Files.copy(defaultImage, imageFolder.toPath(), StandardCopyOption.REPLACE_EXISTING)
     }
 }
